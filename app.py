66ad85dc7a8b6c93a95a16d25db41cd5d1e3907d
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-06-23 23:26:09 +0530

    hello

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, request
-# from Google import Create_Service
-# import base64
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
+from Google import Create_Service
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 app = Flask(__name__)
 
 
-# CLIENT_SECRET_FILE = 'client_secret_id.json'
-# API_NAME = 'gmail'
-# API_VERSION = 'v1'
-# SCOPES = ['https://mail.google.com/']
+CLIENT_SECRET_FILE = 'client_secret_id.json'
+API_NAME = 'gmail'
+API_VERSION = 'v1'
+SCOPES = ['https://mail.google.com/']
 
-# service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
  
 
 @app.route('/api', methods=['GET'])
@@ -35,15 +35,15 @@ def send():
 	print('phone : ' + phone)
 	print('semester : ' + semester)
 	print('branch : ' + branch)
-	# emailMsg = 'Remember ' + name + ' Rafikka Uyir'
-	# mimeMessage = MIMEMultipart()
-	# mimeMessage['to'] = email
-	# mimeMessage['subject'] = 'Rafikka Uyir'
-	# mimeMessage.attach(MIMEText(emailMsg, 'plain'))
-	# raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+	emailMsg = 'Remember ' + name + ' Rafikka Uyir'
+	mimeMessage = MIMEMultipart()
+	mimeMessage['to'] = email
+	mimeMessage['subject'] = 'Rafikka Uyir'
+	mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+	raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
-	# message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-	# print(message)
+	message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+	print(message)
 	return 'Done', 201
 
 if __name__ == '__main__':
